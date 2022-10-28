91940692b5b3de13b14aac3c776b0181ce3b484d
JoshuaOlubori <joshuaolubori@gmail.com>
2022-10-28 13:14:01 +0100

    Finished navbar layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'csGreen': '#247c2c',
+        'csBlack': '#545450',
+        'lightCream': '#fffee6',
+      }
+      
+    },
   },
   plugins: [],
 }
